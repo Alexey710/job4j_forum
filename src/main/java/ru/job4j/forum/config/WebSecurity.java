9b55e39d2204234105b2a8c6d565ac621d9bd4ce
Alexey710 <elsite2015@yandex.ru>
2021-04-28 06:18:16 +0400

    debug heroku

--- src/main/java/ru/job4j/forum/config/WebSecurity.java
+++ src/main/java/ru/job4j/forum/config/WebSecurity.java
@@ -15,7 +15,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
-    @Autowired
+    //@Autowired
     private PasswordEncoder passwordEncoder;
 
     @Autowired
